3aebcbe180524acd453d4e2216e9c2a968fa6ade
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-28 14:04:28 -0700

    app.rb

--- app.rb
+++ app.rb
@@ -1,18 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'twilio-ruby'
-
-# post '/receive_sms' do
-#   content_type 'text/xml'
-#
-#   response = Twilio::TwiML::Response.new do |r|
-#     r.Message "Hey thanks for messaging me!"
-#   end
-#
-#   response.to_xml
-# end
-
 
 get '/' do
-  "Hello World"
+  "Hello from Sinatra on Heroku!"
 end
