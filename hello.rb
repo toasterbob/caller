532b4747f5cdb1825cbf0211bf9a0b9b37511dfc
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-29 11:41:29 -0700

    app.rb

--- hello.rb
+++ hello.rb
@@ -158,5 +158,19 @@ get '/hello-monkey' do
     r.Say "Hello #{name}. #{lyrics2} Goodbye #{name}. This song is for you"
     r.Play "http://www.marknoizumi.com/caller/never_gonna_give_you_up.mp3"
     r.Say "Why are you still here?"
+
+    r.Gather :numDigits => '1', :action => '/hello-monkey/handle-gather', :method => 'get' do |g|
+      #g.Say 'To speak to a real monkey, press 1.'
+      g.Say 'Press any key to start over.'
+    end
+
   end.text
+end
+
+get '/hello-monkey/handle-gather' do
+  redirect '/hello-monkey' #unless params['Digits'] == '1'
+  # Twilio::TwiML::Response.new do |r|
+  #   r.Dial '+13105551212' ### Connect the caller to Koko, or your cell
+  #   r.Say 'The call failed or the remote party hung up. Goodbye.'
+  # end.text
 end
