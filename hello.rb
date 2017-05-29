969d7c5e6fcc17f4680379e6709d49fa3bba994a
Mark Noizumi <mark.noizumi@gmail.com>
2017-05-28 17:08:52 -0700

    app.rb

--- hello.rb
+++ hello.rb
@@ -1,20 +1,42 @@
 require 'sinatra'
 require 'twilio-ruby'
+require 'open-uri'
+require 'json'
 
 get '/' do
   "Hello World!"
+end
+
+def get_advice
+  r = open('http://api.adviceslip.com/advice')
+
+  if r.status[0] == "200"
+    doc = ""
+
+    r.each do |line|
+      doc << line
+    end
+
+    doc = JSON.parse(doc, :symbolize_names => true)
+    advice = doc[:slip][:advice]
+
+    return advice
+  end
+
 end
 
 
 post '/receive_sms' do
   content_type 'text/xml'
   body = params['Body'].downcase
-  
+
   response = Twilio::TwiML::Response.new do |r|
     if body == "hello"
       r.Message "Hi!"
     elsif body == "bye"
       r.message "Goodbye"
+    elsif body == "advice"
+      r.message get_advice
     else
       r.message "Thanks for the crackerjacks!"
     end
